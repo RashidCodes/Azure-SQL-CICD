1a1e0d8721fef46a3a1f4683f7ffb8307f263367
Kingmoh <rashidmincan@gmail.com>
2023-11-03 15:26:54 +1100

    initial commit

--- local_powershell_function/build_powershell_image.sh
+++ local_powershell_function/build_powershell_image.sh
@@ -1,9 +1,10 @@
 #!/bin/bash 
 
 # Build and push image
-docker rmi kingmoh/local_powershell_function:v2;
-docker build --tag kingmoh/local_powershell_function:v2 .
-docker push kingmoh/local_powershell_function:v2
+version="v3"
+docker rmi kingmoh/local_powershell_function:$version;
+docker build --tag kingmoh/local_powershell_function:$version .
+docker push kingmoh/local_powershell_function:$version
 
 # Registry username and password not required (image is public)
-az functionapp config container set --image kingmoh/local_powershell_function:v2 --name localpowershellfunction --resource-group RG-TEST
+az functionapp config container set --image kingmoh/local_powershell_function:$version --name localpowershellfunction --resource-group RG-TEST
