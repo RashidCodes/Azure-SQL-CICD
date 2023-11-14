a0623b5a54f00b26bd87a248f58855bcaee1b466
rashid <rashid.mohammed@mafinancial.com>
2023-11-14 17:09:11 +1100

    Add dacpac location

--- DBProj/VSDBProj/publish.sh
+++ DBProj/VSDBProj/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
 echo "Publishing dacpac"
-DACPAC_LOCATION="$(System.DefaultWorkingDirectory)/_azuresqlcicd/DBProj/VSDBProj/bin/Release/VSDBProj.dacpac"
+DACPAC_LOCATION="./DBProj/VSDBProj/bin/Release/VSDBProj.dacpac"
 target_db_conn_azure_str="Server=${TARGET_SERVER},${TARGET_PORT};Database=${TARGET_DB};Encrypt=True;Authentication=Active Directory MSI;TrustServerCertificate=True;Connection Timeout=360;User Id=${CLIENT_ID};"
 sqlpackage /Action:Publish /SourceFile:${DACPAC_LOCATION} /TargetConnectionString:\"${target_db_conn_azure_str}\" /p:BlockOnPossibleDataLoss=False /p:DropConstraintsNotInSource='False'
