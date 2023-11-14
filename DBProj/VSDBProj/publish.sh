ea8d3b4d27da27dffe44c4b6be06cc59e55df588
Kingmoh <rashidmincan@gmail.com>
2023-11-15 09:50:57 +1100

    Edit publish.sh

--- DBProj/VSDBProj/publish.sh
+++ DBProj/VSDBProj/publish.sh
@@ -2,5 +2,11 @@
 
 echo "Publishing dacpac"
 DACPAC_LOCATION="./DBProj/VSDBProj/bin/Release/VSDBProj.dacpac"
-target_db_conn_azure_str="Server=${TARGET_SERVER},${TARGET_PORT};Database=${TARGET_DB};Encrypt=True;Authentication=Active Directory MSI;TrustServerCertificate=True;Connection Timeout=360;User Id=${CLIENT_ID};"
-sqlpackage /Action:Publish /SourceFile:${DACPAC_LOCATION} /TargetConnectionString:\"${target_db_conn_azure_str}\" /p:BlockOnPossibleDataLoss=False /p:DropConstraintsNotInSource='False'
+
+# For local dev
+# ADJUSTED_DACPAC_LOCATION="./bin/Release/VSDBProj.dacpac"
+# target_db_conn_azure_str="tcp:Server=${TARGET_SERVER},${TARGET_PORT};Initial Catalog=${TARGET_DB};Encrypt=True;Authentication=Active Directory Managed Identity;TrustServerCertificate=True;Connection Timeout=360;User Id=${CLIENT_ID};"
+
+
+# sqlpackage /Action:Publish /SourceFile:${DACPAC_LOCATION} /TargetConnectionString:"Server=tcp:${TARGET_SERVER},${TARGET_PORT};Initial Catalog=${TARGET_DB};Persist Security Info=False;User ID=${CLIENT_ID};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+sqlpackage /Action:Publish /SourceFile:${DACPAC_LOCATION} /TargetConnectionString:"Server=tcp:${TARGET_SERVER},${TARGET_PORT};Initial Catalog=${TARGET_DB};Persist Security Info=False;User ID=${USER_ID};Password=${PASS};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;"
