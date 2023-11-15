636f3f467c413c6a19458c0b5a0d49cd64fdbff7
Kingmoh <rashidmincan@gmail.com>
2023-11-15 16:53:34 +1100

    Remove VSDBProj

--- DBProj/VisualStudioDatabaseProject/publish.sh
+++ DBProj/VisualStudioDatabaseProject/publish.sh
@@ -1,12 +1,5 @@
 #!/bin/bash 
 
 echo "Publishing dacpac"
-DACPAC_LOCATION="/azp/_work/r1/a/_azuresqlcicd/DBProj/VSDBProj/bin/Release/VSDBProj.dacpac"
-
-# For local dev
-# ADJUSTED_DACPAC_LOCATION="./bin/Release/VSDBProj.dacpac"
-# target_db_conn_azure_str="tcp:Server=${TARGET_SERVER},${TARGET_PORT};Initial Catalog=${TARGET_DB};Encrypt=True;Authentication=Active Directory Managed Identity;TrustServerCertificate=True;Connection Timeout=360;User Id=${CLIENT_ID};"
-
-
-# sqlpackage /Action:Publish /SourceFile:${DACPAC_LOCATION} /TargetConnectionString:"Server=tcp:${TARGET_SERVER},${TARGET_PORT};Initial Catalog=${TARGET_DB};Persist Security Info=False;User ID=${CLIENT_ID};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+DACPAC_LOCATION="/azp/_work/r1/a/_azuresqlcicd/DBProj/VisualStudioDatabaseProject/bin/Release/VisualStudioDatabaseProject.dacpac"
 sqlpackage /Action:Publish /SourceFile:${DACPAC_LOCATION} /TargetConnectionString:"Server=tcp:${TARGET_SERVER},${TARGET_PORT};Initial Catalog=${TARGET_DB};Persist Security Info=False;User ID=${USER_ID};Password=${PASS};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;"
